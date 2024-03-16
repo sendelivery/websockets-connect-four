9a3a62747fd64ee20fdd482bde10e60678d6bfe2
Sebastian Chumaceiro <sebcr99@gmail.com>
2024-03-16 18:42:38 +0000

    Refactored app.py to tutorial solution
    Using an iterator instead of modulo expression to cycle between players. Introduced a message type assertion. Narrowed try, except to wrap just the method that can throw.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import asyncio
+import itertools
+import json
 
 import websockets
-
-import json
 
 from connect4 import PLAYER1, PLAYER2, Connect4
 
@@ -13,28 +13,41 @@ async def handler(websocket):
     # Initalise a Connect Four game.
     game = Connect4()
 
+    # Players take alternating turns, using the same browser.
+    turns = itertools.cycle([PLAYER1, PLAYER2])
+    player = next(turns)
+
     async for message in websocket:
+        # Parse a "play" event from the UI.
+        event = json.loads(message)
+        assert event["type"] == "play"
+        column = event["column"]
+
         try:
-            event = json.loads(message)
-
-            player = PLAYER2 if len(game.moves) % 2 else PLAYER1
-            column = event["column"]
+            # Play the move.
             row = game.play(player, column)
-
-            event = {
-                "type": "play",
-                "player": player,
-                "column": column,
-                "row": row,
-            }
-            await websocket.send(json.dumps(event))
-        except RuntimeError:
-            error_event = {"type": "error", "message": "Move is illegal."}
+        except RuntimeError as err:
+            # Send an "error" event if the move was illegal.
+            error_event = {"type": "error", "message": str(err)}
             await websocket.send(json.dumps(error_event))
+            continue
 
+        # Send a "play" event to update the UI.
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+
+        # If move is winning, send a "win" event.
         if game.last_player_won:
             win_event = {"type": "win", "player": game.last_player}
             await websocket.send(json.dumps(win_event))
+
+        # Alternate turns.
+        player = next(turns)
 
 
 async def main():
